027d080bf8d82028377f90696cab9dd3aff8c94a
Bart Wesselink <bartwesselink99@gmail.com>
2016-10-18 19:03:19 +0200

    Added initial test file

---
+++ tests/SOMtodayPHPApiTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bart Wesselink
+ * Date: 18-10-2016
+ * Time: 18:59
+ */
+
+namespace TeamDeployment\SOMtodayPHPApi\SOMtodayPHPApi\Test;
+
+
+class SOMtodayPHPApiTest extends \PHPUnit_Framework_TestCase
+{
+
+}
